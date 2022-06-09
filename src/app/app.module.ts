253bcd17d61a7540753d483c5e2699ca63f6d45b
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-06-09 14:14:55 +0200

    moved content to distinct component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {MatIconModule} from "@angular/material/icon";
-import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import {MainMenuComponent} from './components/main-menu/main-menu.component';
+import {Routes, RouterModule} from "@angular/router";
+import {MainPageComponent} from './components/main-page/main-page.component';
+
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    MainPageComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     MatIconModule
   ],
