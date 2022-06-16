ea19dec4b50e3987c4ac49e6aa4f5f5dce8c9cce
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-06-16 11:27:10 +0200

    new tabs

---
+++ src/app/components/my-git/my-git.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyGitComponent } from './my-git.component';
+
+describe('MyGitComponent', () => {
+  let component: MyGitComponent;
+  let fixture: ComponentFixture<MyGitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MyGitComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MyGitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/my-git/my-git.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-git',
+  templateUrl: './my-git.component.html',
+  styleUrls: ['./my-git.component.css']
+})
+export class MyGitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
