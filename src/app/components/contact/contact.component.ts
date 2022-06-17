8ed50b35e26db2bf40c410a7c7a2d00b187f3265
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-06-17 23:33:31 +0200

    added "mail me" button

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  mailText:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+      this.mailText = "mailto:lukaszmichalak0402@gmail.com?subject=Dev-contact&body=";
   }
-
 }
