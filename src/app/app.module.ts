974f7e8407b89a127b656c84db4c2781de32e47f
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2023-11-23 23:22:52 +0100

    angular 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
-import {AppComponent} from './app.component';
-import {MatIconModule} from "@angular/material/icon";
-import {MainMenuComponent} from './components/main-menu/main-menu.component';
-import {RouterModule, Routes} from "@angular/router";
-import {MainPageComponent} from './components/main-page/main-page.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AboutMeComponent} from './components/about-me/about-me.component';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
-import {ContactComponent} from './components/contact/contact.component';
-import {MyGitComponent} from './components/my-git/my-git.component';
-import {MatLegacyCardModule as MatCardModule} from "@angular/material/legacy-card";
-import {HttpClientModule} from '@angular/common/http';
-import {MatLegacyTableModule as MatTableModule} from "@angular/material/legacy-table";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { ContactComponent } from './components/contact/contact.component';
+import { MyGitComponent } from './components/my-git/my-git.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 const routes: Routes = [
   {path: 'main-page', component: MainPageComponent},
