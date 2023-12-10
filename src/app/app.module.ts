f7d3f373316cc062387ed79c6aff5a84d9f87ffa
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2023-12-10 13:20:08 +0100

    changed pages around

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from './main-page/main-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutMeComponent } from './about-me/about-me.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ContactComponent } from './contact/contact.component';
@@ -16,10 +14,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MyGitComponent } from './my-git/components/my-git.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const routes: Routes = [
-  {path: 'main-page', component: MainPageComponent},
-  {path: 'about-me', component: AboutMeComponent},
   {path: 'my-git', component: MyGitComponent},
   {path: 'contact', component: ContactComponent},
   {path: '', redirectTo: 'main-page', pathMatch: 'full'},
@@ -30,8 +27,6 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     MainMenuComponent,
-    MainPageComponent,
-    AboutMeComponent,
     ContactComponent,
     MyGitComponent
   ],
@@ -43,14 +38,13 @@ const routes: Routes = [
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
     MatButtonModule,
     MatCardModule,
     MatTableModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
